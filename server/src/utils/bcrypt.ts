37fb8d54680828779439405be0e314b3ea4712fb
ritikdoijod <ritikdoijod@gmail.com>
2025-02-04 23:30:22 +0530

    added hash and compare functions

---
+++ server/src/utils/bcrypt.ts
@@ -0,0 +1,5 @@
+import bcrypt from "bcrypt"
+
+export const hashValue = async (value: string, saltRounds: number = 10) => await bcrypt.hash(value, saltRounds);
+
+export const compareValue = async (value: string, hasedValue: string) => await bcrypt.compare(value, hasedValue)
