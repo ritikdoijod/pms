762046674a1fafe388e0d57ee8b0bf93267fff5b
ritikdoijod <ritikdoijod@gmail.com>
2025-02-05 22:43:42 +0530

    added roles, task and member models

--- server/src/models/user.models.ts
+++ server/src/models/user.models.ts
@@ -12,46 +12,48 @@ export interface UserDocument extends Document {
   updatedAt: Date;
   currentWorkspace: mongoose.Types.ObjectId | null;
   comparePassword(value: string): Promise<boolean>;
-  omitPassword(): Omit<UserDocument, "password">
+  omitPassword(): Omit<UserDocument, "password">;
 }
 
-const userSchema = new Schema<UserDocument>({
-  name: {
-    type: String,
-    required: false,
-    trim: true
+const userSchema = new Schema<UserDocument>(
+  {
+    name: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      select: true,
+    },
+    profilePicture: {
+      type: String,
+      default: null,
+    },
+    currentWorkspace: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Workspace",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true
+  {
+    timestamps: true,
   },
-  password: {
-    type: String,
-    select: true
-  },
-  profilePicture: {
-    type: String,
-    default: null
-  },
-  currentWorkspace: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Workspace"
-  },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-  lastLogin: {
-    type: Date,
-    default: null
-  }
-}, {
-  timestamps: true
-})
-
+);
 
 userSchema.pre("save", async function (next) {
   if (this.isModified("password")) {
@@ -60,18 +62,18 @@ userSchema.pre("save", async function (next) {
     }
   }
   next();
-})
+});
 
 userSchema.methods.omitPassword = function (): Omit<UserDocument, "password"> {
   const userObject = this.toObject();
   delete userObject.password;
-  return userObject
-}
+  return userObject;
+};
 
 userSchema.methods.comparePassword = async function (value: string) {
-  return compareValue(value, this.password)
-}
+  return compareValue(value, this.password);
+};
 
-const User = mongoose.model<UserDocument>("User", userSchema)
+const User = mongoose.model<UserDocument>("User", userSchema);
 
-export default User
+export default User;
