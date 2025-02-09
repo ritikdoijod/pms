b22e0f89e07396b167e499a12215980c06c4dd92
ritikdoijod <ritikdoijod@gmail.com>
2025-02-09 19:32:00 +0530

    added local strategy

--- server/src/configs/passport.config.ts
+++ server/src/configs/passport.config.ts
@@ -1,9 +1,13 @@
 import passport from "passport";
 import { Request } from "express";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as LocalStrategy } from "passport-local";
 import { config } from "./app.config";
 import { NotFoundException } from "../utils/appError";
-import { loginOrCreateAccountService } from "../services/auth.service";
+import {
+  loginOrCreateAccountService,
+  verifyUserService,
+} from "../services/auth.service";
 import { ProviderEnum } from "../enums/account-provider.enum";
 
 passport.use(
@@ -33,6 +37,24 @@ passport.use(
         done(null, user);
       } catch (error) {
         done(error, false);
+      }
+    },
+  ),
+);
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      session: true,
+    },
+    async (email, password, done) => {
+      try {
+        const user = await verifyUserService({ email, password });
+        return done(null, user);
+      } catch (error: any) {
+        return done(error, false, { message: error?.message });
       }
     },
   ),
